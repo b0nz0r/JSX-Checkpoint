33035c9a7f6ebc955c66473f67b243e9079fb660
b0nz0r <bahabdira123@outlook.fr>
2024-01-15 20:30:07 +0100

    JSX CheckPoint

---
+++ src/product.js
@@ -0,0 +1,9 @@
+// src/product.js
+let product = {
+  name: "CONTROLLER",
+  price: "69 $",
+  description: "Description du produit",
+  imageUrl: "https://apibackend.megapc.tn//uploads/gallerie/1695202546010.webp",
+};
+
+export default product;
